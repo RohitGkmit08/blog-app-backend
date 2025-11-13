c91afb642414885294ca84757b2634eba29e55f5
Rohit Sinha <your-email@used-on-github.com>
2025-11-13 17:31:24 +0530

    feat(admin-auth): add admin authentication module

---
+++ routes/adminRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const adminRouter = express.Router();
+
+const { adminLogin } = require("../controllers/adminController");
+
+adminRouter.post("/login", adminLogin);
+
+
+module.exports = adminRouter;
